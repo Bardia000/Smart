722ab9b44b16187db9483e132ddd382c9fdc8446
aliebadi65 <aliebadi66@gmail.com>
2016-05-22 14:05:17 +0430

    Smart v3.8

--- plugins/addsudo.lua
+++ plugins/addsudo.lua
@@ -10,6 +10,7 @@ end
     local user = 'user#136141698'
     local chat = 'chat#id'..msg.to.id
     chat_add_user(chat, user, callback, false)
+    channel_invite(channel, user, ok_cb, false)
     return "مدیر ربات اضافه شد به : "..string.gsub(msg.to.print_name, "_", " ")..'\n'..msg.to.id..''
 end
 return {
