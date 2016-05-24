8e2273d57585658607ea94cb5d765a9bc28413fe
aliebadi65 <aliebadi66@gmail.com>
2016-05-25 00:02:17 +0430

    Smart v3.8

--- plugins/antilink.lua
+++ plugins/antilink.lua
@@ -2,20 +2,12 @@
 local function run(msg)
     
     local data = load_data(_config.moderation.data)
-    
-     if data[tostring(msg.to.id)]['settings']['lock_adds'] == 'yes' then
-      
-    
 if string.match(value, names[i]) and not is_momod(msg) then
 	if msg.to.type == 'channel' then
 	delete_msg(msg.id,ok_cb,false)
-    
-    
 chat_del_user('chat#id'..msg.to.id, 'user#id'..msg.from.id, ok_cb, true)
    local receiver = msg.to.id
     send_large_msg('chat#id'..receiver, msg.."\n", ok_cb, false)
-	
-      end
    end
 end
     
